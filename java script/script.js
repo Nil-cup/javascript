fbc8d31161f2cdc8f4c11db547766126c594caf0
Nil-cup <nsastre20625@iesjoanramis.org>
2024-10-14 10:43:13 +0200

    Add files via upload

---
+++ java script/script.js
@@ -0,0 +1,28 @@
+function suma (){
+    let n1 = parseFloat(prompt("Dime un numero"));
+    let n2 = parseFloat(prompt("Dime otro numnero"));
+    document.write(n1+n2)
+}
+
+function resta (){
+    let n1 = parseFloat(prompt("Dime un numero"));
+    let n2 = parseFloat(prompt("Dime otro numnero"));
+    document.write(n1-n2)
+}
+
+function mult (){
+    let n1 = parseFloat(prompt("Dime un numero"));
+    let n2 = parseFloat(prompt("Dime otro numnero"));
+    document.write(n1*n2)
+}
+
+function div (){
+    let n1 = parseFloat(prompt("Dime un numero"));
+    let n2 = parseFloat(prompt("Dime otro numnero"));
+    document.write(n1/n2)
+}
+function arrel(){
+    let n1 = parseFloat(prompt("Dime un numero"));
+    let n2 = parseFloat(prompt("Dime otro numnero"));
+    document.write(Math.sqrt(n1))
+}
